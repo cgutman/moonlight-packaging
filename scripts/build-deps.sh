a7075f312729c504909cb28c63ab639c805f43d8
Cameron Gutman <aicommander@gmail.com>
2020-12-22 18:13:45 -0600

    Update to SDL 2.0.14

--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -27,20 +27,16 @@ else
 fi
 
 # Build and install our SDL2 build
-# Since we'll be linking statically, we need to use Libs.private instead of Libs, but
-# there's no way to tell QMake to use the Libs.private section, so we have to replace
-# Libs with Libs.private ourselves prior to installation.
 cd /opt/SDL2
 ./configure --enable-static --disable-shared --enable-video-kmsdrm --disable-video-rpi
 make -j$(nproc)
-sed -i '/^Libs:/d' sdl2.pc
-sed -i 's/Libs.private:/Libs:/g' sdl2.pc
 cat sdl2.pc
 make install
 
 # Build and install SDL_ttf
-# For similar reasons to SDL2 above, we have to patch the pkgconfig file in order to
-# link with a static library.
+# Since we'll be linking statically, we need to use Libs.private instead of Libs, but
+# there's no way to tell QMake to use the Libs.private section, so we have to replace
+# Libs with Libs.private ourselves prior to installation.
 cd /opt/SDL_ttf
 ./configure --enable-static --disable-shared
 make -j$(nproc)
