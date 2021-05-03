0eb92f1f4a284c5703c19f0383981cd005301d2d
Cameron Gutman <aicommander@gmail.com>
2021-05-03 22:41:04 +0000

    Add do-release.sh

---
+++ do-release.sh
@@ -0,0 +1,24 @@
+fail()
+{
+	echo "$1" 1>&2
+	exit 1
+}
+
+git diff-index --quiet HEAD -- || fail "Release builds must not have unstaged changes!"
+
+set -e
+
+# Ensure build images are pushed to DockerHub
+./push-images.sh
+
+# Build the packages
+./build-l4t.sh
+./build-rpi.sh
+
+# Push the moonlight-qt packages to Cloudsmith repos
+cloudsmith push deb moonlight-game-streaming/moonlight-l4t/ubuntu/bionic out_l4t-bionic/moonlight-qt_*.deb
+cloudsmith push deb moonlight-game-streaming/moonlight-qt/raspbian/buster out_rpi-buster/moonlight-qt_*.deb
+
+# Push the moonlight-qt-dbgsym packages to Cloudsmith repos
+cloudsmith push deb moonlight-game-streaming/moonlight-l4t/ubuntu/bionic out_l4t-bionic/moonlight-qt-dbgsym_*.ddeb
+cloudsmith push deb moonlight-game-streaming/moonlight-qt/raspbian/buster out_rpi-buster/moonlight-qt-dbgsym_*.deb
