52349e9f64af8fdc9563525806ab4865c4f6c518
Cameron Gutman <aicommander@gmail.com>
2025-07-27 14:40:06 -0500

    Upgrade from Qt 5 to Qt 6

--- scripts/install-base-deps.sh
+++ scripts/install-base-deps.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get install -y curl g++ make ninja-build devscripts fakeroot debhelper git nasm libgbm-dev libdrm-dev libasound2-dev libdbus-1-dev libegl1-mesa-dev libgl1-mesa-dev libgles2-mesa-dev libglu1-mesa-dev libibus-1.0-dev libpulse-dev libudev-dev libx11-dev libxcursor-dev libxext-dev libxi-dev libxinerama-dev libxkbcommon-dev libxrandr-dev libxss-dev libxt-dev libxv-dev libxxf86vm-dev cmake libgl-dev meson wayland-protocols libwayland-dev libdecor-0-dev libfreetype-dev libpipewire-0.3-dev
-apt-get install -y --no-install-recommends libssl-dev qtbase5-dev qtquickcontrols2-5-dev qtdeclarative5-dev libqt5svg5-dev libopus-dev gnupg
+apt-get install -y --no-install-recommends libssl-dev qt6-base-dev qt6-declarative-dev libqt6svg6-dev libopus-dev gnupg
 
 # Install FFmpeg if we're using the OS-provided version
 if [ "$TARGET" == "rpi" ] || [ "$TARGET" == "rpi64" ]; then
